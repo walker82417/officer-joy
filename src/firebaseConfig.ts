df97c0fe91a05ec4129cb0420fb42148b90fe9ea
Rohan Doiphode <136227230+walker82417@users.noreply.github.com>
2026-07-14 08:34:19 +0530

    Fix formatting of googleProvider export

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,4 +1,3 @@
-// src/firebaseConfig.ts
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
